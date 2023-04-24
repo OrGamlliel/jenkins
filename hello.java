ce7184ec826cb7c47d6c34fffba231990f3a5aeb
Or Gamliel <96204149+OrGamlliel@users.noreply.github.com>
2023-04-24 19:29:27 +0300

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,7 +1,7 @@
 public class Hello{
 	public static void main(String[] args) {
 		for (int i=1;i<=10; i++) {
-			System.out.println("Hi moshe ..."+i);
+			System.out.println("hi moshe ..."+i);
 			}
 		}
 	}
